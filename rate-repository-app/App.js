d585675ec967d644927acf627620d9b343e80445
tommivk <tommikokk@gmail.com>
2022-05-30 01:18:52 +0300

    add repository list

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
+import RepositoryList from "./src/components/RepositoryList";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RepositoryList />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>Name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Fork count: {item.forksCount}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
